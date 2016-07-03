dac87a56d1742a063d4225a49551afa0c4e7a5de
Gustavo <gustavosabonare@hotmail.com>
2016-07-03 17:42:50 -0300

    All the Models

---
+++ Imob.Site/Models/Rent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Imob.Site.Models
+{
+    public class Rent
+    {
+        public int ID { get; set; }
+
+    }
+}
