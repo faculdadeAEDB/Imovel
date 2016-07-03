0e808352e92081d47d02bf080f7789100dcfd2e8
Gustavo <gustavosabonare@hotmail.com>
2016-07-03 17:53:25 -0300

    Payment and Rent Models

--- Imob.Site/Models/Payment.cs
+++ Imob.Site/Models/Payment.cs
@@ -7,5 +7,11 @@ namespace Imob.Site.Models
 {
     public class Payment
     {
+        public int ID { get; set; }
+        public string DataPagament { get; set; }
+        public Rent Alguel { get; set; }
+
+        public ICollection<Fee> Taxas { get; set; }
+
     }
 }
--- Imob.Site/Models/Rent.cs
+++ Imob.Site/Models/Rent.cs
@@ -8,6 +8,13 @@ namespace Imob.Site.Models
     public class Rent
     {
         public int ID { get; set; }
+        public decimal Valor { get; set; }
+        public string DataInicio { get; set; }
+        public string DataFim { get; set; }
+        public Customer Inquilino { get; set; }
+        public Owner Dono { get; set; }
+
+        public ICollection<Payment> Pagamentos { get; set; }
 
     }
 }
