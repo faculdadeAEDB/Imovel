0e8b9d2b807b0aaa11f77eb4dd60ed35b379b4f6
xablau <xablau@xabablau.com>
2016-07-03 18:08:43 -0300

    Create dataset

--- Imob.Site/Models/Context/Context.cs
+++ Imob.Site/Models/Context/Context.cs
@@ -8,6 +8,13 @@ namespace Imob.Site.Models.Context
 {
     public class Context : DbContext
     {
+        public DbSet<Customer> Cliente { get; set; }
+        public DbSet<Fee> Taxa { get; set; }
+        public DbSet<Habitation> Propriedade { get; set; }
+        public DbSet<Owner> Proprietario { get; set; }
+        public DbSet<Payment> Pagamento { get; set; }
+        public DbSet<Rent> Aluguel { get; set; }
+
 
     }
 }
