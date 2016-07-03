b563710c309b38e3c4b3012bfca03cad3c1bf6b4
Gustavo <gustavosabonare@hotmail.com>
2016-07-03 20:21:46 -0300

    All repositories

---
+++ Imob.Site/Models/Repository/CustomerRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Imob.Site.Models.Repository
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        public Customer Editar(Customer obj)
+        {
+            using (Context.Context c = new Context.Context())
+            {
+                c.Cliente.Attach(obj);
+                c.Entry(obj).State = EntityState.Modified;
+                c.SaveChanges();
+
+                return obj;
+            }
+        }
+
+        public Customer Excluir(Customer obj)
+        {
+            using (Context.Context c = new Context.Context())
+            {
+                Customer clienteExcluido = c.Cliente.Remove(obj);
+                c.SaveChanges();
+                return clienteExcluido;
+            }
+        }
+
+        public List<Customer> Obter()
+        {
+            using (Context.Context c = new Context.Context())
+            {
+                return c.Cliente.ToList();
+            }
+        }
+
+        public Customer Salvar(Customer obj)
+        {
+            using (Context.Context c = new Context.Context())
+            {
+                Customer novoCliente = c.Cliente.Add(obj);
+                c.SaveChanges();
+                return novoCliente;
+            }
+        }
+    }
+}
---
+++ Imob.Site/Models/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imob.Site.Models.Repository
+{
+    public interface IRepository<T>
+    {
+        List<T> Obter();
+        T Salvar(T obj);
+        T Editar(T obj);
+        T Excluir(T obj);
+
+    }
+}
