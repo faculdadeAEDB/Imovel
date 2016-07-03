a2f5c100b571de0ba0ec1fc08836bc93b8ef56eb
xablau <xablau@xabablau.com>
2016-07-03 17:13:38 -0300

    First or Second

---
+++ Imob.Site/Models/Context/Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Imob.Site.Models.Context
+{
+    public class Context : DbContext
+    {
+    }
+}
