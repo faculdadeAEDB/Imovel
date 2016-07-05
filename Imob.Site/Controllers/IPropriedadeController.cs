ae8fd0028dd9390409825919684d1f792ac182c8
xablau <xablau@xabablau.com>
2016-07-04 21:27:04 -0300

    Create controller files

---
+++ Imob.Site/Controllers/IPropriedadeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Imob.Site.Models;
+
+namespace Imob.Site.Controllers
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPropriedadeController" in both code and config file together.
+    [ServiceContract]
+    public interface IPropriedadeController
+    {
+        [OperationContract]
+        Habitation Salvar(Habitation obj);
+
+        [OperationContract]
+        List<Habitation> Obter();
+
+        [OperationContract]
+        Habitation Editar(Habitation obj);
+
+        [OperationContract]
+        Habitation Excluir(Habitation obj);
+    }
+}
---
+++ Imob.Site/Controllers/PropriedadeController.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Imob.Site.Models;
+using Imob.Site.Models.Repository;
+
+namespace Imob.Site.Controllers
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PropriedadeController" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PropriedadeController.svc or PropriedadeController.svc.cs at the Solution Explorer and start debugging.
+    public class PropriedadeController : IPropriedadeController
+    {
+        private HabitationRepository repository = new HabitationRepository();
+        public Habitation Editar(Habitation obj)
+        {
+            return repository.Editar(obj);
+        }
+
+        public Habitation Excluir(Habitation obj)
+        {
+            return repository.Excluir(obj);
+        }
+
+        public List<Habitation> Obter()
+        {
+            return repository.Obter();
+        }
+
+        public Habitation Salvar(Habitation obj)
+        {
+            return repository.Salvar(obj);
+        }
+    }
+}
